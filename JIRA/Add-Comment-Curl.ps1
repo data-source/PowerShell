665162982508c1912c13fec5722390fa9668d275
itokamak <itokamak@me.com>
2017-06-23 11:34:02 +0100

    Create Add-Comment-Curl.ps1

---
+++ JIRA/Add-Comment-Curl.ps1
@@ -0,0 +1,9 @@
+function JiraComment
+{
+param( [string]$issue, [string]$comment)
+"Calling JiraComment with variables | issue = " + $issue + " And Text = " + $comment
+[string]$cmd1 = '.\curl --%  -D- -u user:password -X PUT -d "{\"update\": {\"comment\": [{\"add\": {\"body\":\"' + $comment+ '\"}}]}}"'
+[string]$cmd2 = ' -H "Content-Type: application/json" https://jira.company.com/rest/api/2/issue/{0}' -f $issue;
+$all_cmds = "$cmd1 $cmd2 "
+Invoke-Expression $all_cmds
+}
